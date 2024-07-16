9c668fd38a42927a41c5f1062a568bc67e45bd18
Aleksandr Abramov <aleksandr.a@findbride.com>
2024-07-17 03:26:00 +0600

    03 added readUserName

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,4 +1,9 @@
 #!/usr/bin/env node
+import {readUserName} from "../src/cli.js";
+import {sayHello} from "../src/utils.js";
 
 console.log("node bin/brain-games.js");
 console.log("Welcome to the Brain Games!")
+const name = readUserName()
+sayHello(name)
+
